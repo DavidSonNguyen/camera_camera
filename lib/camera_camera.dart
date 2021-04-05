e33af01e38fc71b1ae7f9811e926a9a0872a6d1d
DavidSon Nguyen <gnvisons@gmail.com>
2021-04-06 00:22:51 +0700

    Commit export

--- lib/camera_camera.dart
+++ lib/camera_camera.dart
@@ -2,6 +2,5 @@ library camera_camera;
 
 export 'src/presentation/camera_page.dart';
 export 'src/shared/entities/camera_side.dart';
-export 'package:camera/camera.dart';
 export 'package:camera_camera/src/presentation/controller/camera_camera_controller.dart';
 export 'package:camera_camera/src/core/camera_bloc.dart';
